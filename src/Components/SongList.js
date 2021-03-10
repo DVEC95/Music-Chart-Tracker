004ee99d766a6d9f5f01fc484180c1f9b6464287
David Eastcroft <davideastcroft95@gmail.com>
2021-03-10 20:00:40 +0000

    Changes function back.

--- src/Components/SongList.js
+++ src/Components/SongList.js
@@ -3,7 +3,7 @@ import Song from "./Song"
 
 const SongList = (props) => {
 
-  const songs = props.songs.map(function(song, index){
+  const songs = props.songs.map((song, index) => {
     if(props.songs){
       return(
         <Song key={index} chartPosition={index + 1} artist={song["im:artist"].label} title={song["im:name"].label} image={song["im:image"][2].label} preview={song.link[1].attributes.href}/>
